4200ffa58fc04027a372cdcb32f17323dccee444
Dmitriy <chekanov.d@gmail.com>
2020-04-02 14:08:40 +0300

    Small syntax changes

--- 1_task/index.js
+++ 1_task/index.js
@@ -1,51 +1,51 @@
 /*
-	TASK 1.1
-	Write a program which reads a string from the standard input stdin, reverses it and then writes it to the standard output stdout.
-		•The program should be started from npmscript via nodemon(i.e. npm run task1).
-		•The program should be running in a stand-by mode and should not be terminated after the first-stringprocessing.
+  TASK 1.1
+  Write a program which reads a string from the standard input stdin, reverses it and then writes it to the standard output stdout.
+    •The program should be started from npmscript via nodemon(i.e. npm run task1).
+    •The program should be running in a stand-by mode and should not be terminated after the first-stringprocessing.
 */
 
 
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+/* const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// rl.on('line', (input) => {
-//   console.log(input.split("").reverse().join(""));
-// });
+rl.on('line', (input) => {
+  console.log(input.split("").reverse().join(""));
+});*/
+
 
 /*
-	TASK 1.2 
-	Write a program which should do the following:
+  TASK 1.2 
+  Write a program which should do the following:
     •Read the content of csvfile from./csvdirectory. Example: https://epa.ms/nodejs19-hw1-ex1
     •Use the csvtojsonpackage (https://github.com/Keyang/node-csvtojson) to convert csvfile to jsonobject.
-		•Write the csvfile content to a new txtfile.Use the following format: https://epa.ms/nodejs19-hw1-ex2.
-		•Do not load all the content of the csvfile into RAM via stream (read/write file content line by line).
-		•In case of read/write errors, log them in the console.
+    •Write the csvfile content to a new txtfile.Use the following format: https://epa.ms/nodejs19-hw1-ex2.
+    •Do not load all the content of the csvfile into RAM via stream (read/write file content line by line).
+    •In case of read/write errors, log them in the console.
 */
 
 const fs = require('fs');
 const filePath = './csv/data.csv';
 
 // Example one 
-// const csvtojson = require('csvtojson');
-// csvtojson({
-//   trim: true,
-//   delimiter: ';',
-
-// })
-//   .fromFile(filePath)
-//   .then((jsonObj) => fs.writeFileSync("./data.json",JSON.stringify(jsonObj)))
-//   .catch((error) => console.log(error.message));
-
+/* const csvtojson = require('csvtojson');
+csvtojson({
+  trim: true,
+  delimiter: ';',
+})
+  .fromFile(filePath)
+  .then((jsonObj) => fs.writeFileSync("./data.json",JSON.stringify(jsonObj)))
+  .catch((error) => console.log(error.message));
+ */
 
 // Example two
-// fs.readFile(filePath,'utf8',(error,data) => {
-//   if (error) throw error.message;
-//   fs.writeFileSync("./data.json",data);
-// })
+/* fs.readFile(filePath,'utf8',(error,data) => {
+  if (error) throw error.message;
+  fs.writeFileSync("./data.json",data);
+}) */
 
 // Example three 
 const readline = require('readline');
